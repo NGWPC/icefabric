28c749a4c927acff5666a011486abb5ae4a035ad
Tadd Bindas <Tadd.N.Bindas@rtx.com>
2025-08-06 14:44:08 -0400

    PI-5 Subcase 1: Ingesting, Storing, and Categorizing HEC-RAS Models (#79)
    * initial commit: changed library to iceberg, created a conus_reference pyiceberg table
    
    * feat: added Mike's code from mip_riverML for conflating flowlines to the hydrofabric
    
    * fix: added a comment to the confate func
    
    * feat: added script to write conflated XS to the icefabric catalog
    
    * feat: changed remote_builds to production, added download scripts, edited docs and R code
    
    * patch: removed debugging lines
    
    * Update tools/iceberg/hydrofabric/download_hydrofabric_gpkg.py
    
    Co-authored-by: Quercus Hamlin <75846376+quercoak@users.noreply.github.com>
    
    * fix: addressing PR comments
    
    ---------
    
    Co-authored-by: Quercus Hamlin <75846376+quercoak@users.noreply.github.com>

--- src/icefabric/schemas/__init__.py
+++ src/icefabric/schemas/__init__.py
@@ -1,6 +1,7 @@
 """Contains helper functions to support NWM modules"""
 
 from .hydrofabric import UPSTREAM_VPUS, HydrofabricDomains, IdType
+from .iceberg_tables.ras_xs import ExtractedRasXS
 from .modules import (
     CFE,
     LASAM,
@@ -24,6 +25,7 @@ from .ras_xs import XsType
 from .topobathy import FileType, NGWPCLocations, NGWPCTestLocations
 
 __all__ = [
+    "ExtractedRasXS",
     "UPSTREAM_VPUS",
     "IdType",
     "HydrofabricDomains",

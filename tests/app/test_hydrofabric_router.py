6ddf73e076de356bd63515997cd0295db69f8ac1
Brock Hinkson <brock.w.hinkson@rtx.com>
2025-10-28 15:14:11 -0500

    NGWPC-8514 - Decouple hydrofabric router tests from S3 access

--- tests/app/test_hydrofabric_router.py
+++ tests/app/test_hydrofabric_router.py
@@ -2,19 +2,19 @@ import pytest
 
 
 @pytest.mark.slow
-def test_subset_hl_uri_good(hydrofabric_client, gauge_hf_uri_good: str):
+def test_subset_hl_uri_good(client, watershed_bound_id_good: str):
     """Test: /v1/hydrofabric/{gauge_hf_uri_good}/gpkg"""
-    response = hydrofabric_client.get(
-        f"/v1/hydrofabric/{gauge_hf_uri_good}/gpkg?id_type=hl_uri&domain=conus_hf&layers=divides&layers=flowpaths&layers=network&layers=nexus"
+    response = client.get(
+        f"/v1/hydrofabric/{watershed_bound_id_good}/gpkg?id_type=id&domain=hi_hf&layers=divides&layers=flowpaths&layers=network&layers=nexus"
     )
     assert response.status_code == 200, f"Request failed with status {response.status_code}: {response.text}"
 
 
 @pytest.mark.slow
-def test_subset_hl_uri_bad(hydrofabric_client, gauge_hf_uri_bad: str):
+def test_subset_hl_uri_bad(client, watershed_bound_id_bad: str):
     """Test: /v1/hydrofabric/{gauge_hf_uri_bad}/gpkg"""
-    response = hydrofabric_client.get(
-        f"/v1/hydrofabric/{gauge_hf_uri_bad}/gpkg?id_type=hl_uri&domain=conus_hf&layers=divides&layers=flowpaths&layers=network&layers=nexus"
+    response = client.get(
+        f"/v1/hydrofabric/{watershed_bound_id_bad}/gpkg?id_type=id&domain=hi_hf&layers=divides&layers=flowpaths&layers=network&layers=nexus"
     )
     assert response.status_code == 404, (
         f"Request did not fail as expected. Status: {response.status_code}: {response.text}"
@@ -22,16 +22,16 @@ def test_subset_hl_uri_bad(hydrofabric_client, gauge_hf_uri_bad: str):
 
 
 @pytest.mark.slow
-def test_hl_history_good(hydrofabric_client):
+def test_hl_history_good(client):
     """Test: /v1/hydrofabric/history"""
-    response = hydrofabric_client.get("/v1/hydrofabric/history?domain=conus_hf")
+    response = client.get("/v1/hydrofabric/history?domain=hi_hf")
     assert response.status_code == 200, f"Request failed with status {response.status_code}: {response.text}"
 
 
 @pytest.mark.slow
-def test_hl_history_bad(hydrofabric_client):
+def test_hl_history_bad(client):
     """Test: /v1/hydrofabric/history"""
-    response = hydrofabric_client.get("/v1/hydrofabric/history?domain=prvi_hf")
+    response = client.get("/v1/hydrofabric/history?domain=prvi_hf")
     assert response.status_code == 404, (
         f"Request did not fail as expected. Status: {response.status_code}: {response.text}"
     )

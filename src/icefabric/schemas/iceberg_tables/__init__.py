408ef8604b472b15e446c3460109d267e3b0b113
Brock Hinkson <brock.w.hinkson@rtx.com>
2025-12-01 16:27:35 -0600

    NGWPC-9074 - Add NHF to Iceberg, update build script, update Streamlit dashboard (#28)

---
+++ src/icefabric/schemas/iceberg_tables/__init__.py
@@ -0,0 +1,23 @@
+from .hydrofabric_update import (
+    Divides,
+    Flowpaths,
+    Gages,
+    Hydrolocations,
+    Nexus,
+    ReferenceFlowpaths,
+    VirtualFlowpaths,
+    VirtualNexus,
+    Waterbodies,
+)
+
+nhf_layers = {
+    "divides": Divides,
+    "flowpaths": Flowpaths,
+    "nexus": Nexus,
+    "reference_flowpaths": ReferenceFlowpaths,
+    "waterbodies": Waterbodies,
+    "gages": Gages,
+    "virtual_flowpaths": VirtualFlowpaths,
+    "virtual_nexus": VirtualNexus,
+    "hydrolocations": Hydrolocations,
+}
